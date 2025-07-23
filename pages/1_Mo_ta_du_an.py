26717e7eacb8920330dbb353ff40b609afe50ca8
ngodongnguyen <ngodongnguyen27@gmail.com>
2025-07-23 17:18:06 +0700

    đổi model

--- pages/1_Mo_ta_du_an.py
+++ pages/1_Mo_ta_du_an.py
@@ -45,7 +45,7 @@ Dưới đây là danh sách URL:
 """
             try:
                 response = openai.ChatCompletion.create(
-                    model="gpt-4",
+                    model="gpt-3.5-turbo",
                     messages=[{"role": "user", "content": prompt}],
                     temperature=0.3,
                     max_tokens=1500,
